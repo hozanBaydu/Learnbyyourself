9bd3d35c7e422db85ec10cd122b33ce9169a8ed0
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-10 20:59:22 +0300

    mainactivitynin düzenlenmesi

--- app/src/main/java/com/hozanbaydu/learnbyyourself/views/MainActivity.kt
+++ app/src/main/java/com/hozanbaydu/learnbyyourself/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hozanbaydu.learnbyyourself
+package com.hozanbaydu.learnbyyourself.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hozanbaydu.learnbyyourself.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
