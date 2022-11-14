170479f87af4c5c913f0a7f1f175b34b6cfe17a6
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-14 21:53:58 +0300

    mainactivite inject edilmesi

--- app/src/main/java/com/hozanbaydu/learnbyyourself/views/MainActivity.kt
+++ app/src/main/java/com/hozanbaydu/learnbyyourself/views/MainActivity.kt
@@ -3,10 +3,19 @@ package com.hozanbaydu.learnbyyourself.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hozanbaydu.learnbyyourself.R
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var fragmentFactory: FragmentFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.fragmentFactory=fragmentFactory
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
     }
 }
