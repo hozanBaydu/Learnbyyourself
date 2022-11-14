58cec8d1eec964e916816d64724179584fa827d1
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-14 21:49:06 +0300

    Choicefragmeent kodlarının bitirilmesi

--- app/src/main/java/com/hozanbaydu/learnbyyourself/views/ChoiceFragment.kt
+++ app/src/main/java/com/hozanbaydu/learnbyyourself/views/ChoiceFragment.kt
@@ -1,7 +1,39 @@
 package com.hozanbaydu.learnbyyourself.views
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.hozanbaydu.learnbyyourself.R
+import com.hozanbaydu.learnbyyourself.databinding.FragmentChoiceBinding
+import com.hozanbaydu.learnbyyourself.databinding.FragmentWordBinding
 
 class ChoiceFragment:Fragment(R.layout.fragment_choice) {
+
+    private var fragmentBinding : FragmentChoiceBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentChoiceBinding.bind(view)
+        fragmentBinding = binding
+
+
+        binding.chooseImageview.setOnClickListener {
+
+            findNavController().navigate(ChoiceFragmentDirections.actionChoiceFragmentToApiFragment())
+        }
+
+        binding.drawingImageview.setOnClickListener {
+            findNavController().navigate(ChoiceFragmentDirections.actionChoiceFragmentToDrawingFragment())
+        }
+
+    }
+
+
+    override fun onDestroyView() {
+        fragmentBinding = null
+        super.onDestroyView()
+    }
+
 }
